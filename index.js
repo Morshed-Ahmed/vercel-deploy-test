35bed0168ac49ac41a4a9cdf1a4b4cd2a623fd01
morshedAhmed <morshed50r@gmail.com>
2022-09-20 07:55:29 -0700

    produt route add commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 5050;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/product", (req, res) => {
+  res.send("This is product route");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
